4ae2e6972b1c29985843143111d81ccf70cf7c95
Nils <no.rudnik@googlemail.com>
2024-02-10 12:31:48 +0100

    Frontend Final

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,17 +1,23 @@
+//global variables
 let savedNickname;
-let chosenlanguage = "de";
+let chosenlanguage = "en";
 let socket;
 
+/**
+ * This method sends the text, which has been input, to the server along with the necassary
+ * information in the JSON format
+ */
 function buttonSendAction() {
-    const eingabeFenster = document.getElementById("chatTextEingabe");
-    let eingabeText = eingabeFenster.value;
 
-    if(eingabeText === '' || savedNickname ==='') {
+    const inputWindow = document.getElementById("chatTextInput");
+    let inputText = inputWindow.value;
+
+    if(inputText === '' || savedNickname ==='') {
         alert("Bitte Nachricht eingeben!")
     }else {
-        // Entfernen Sie jede direkte Anzeige im Chatfenster hier.
-        eingabeFenster.value = ""; // Eingabefeld leeren
+        inputWindow.value = "";
 
+        //creating timestamp
         const now = new Date();
 
         var hour = now.getHours().toString().padStart(2, '0');
@@ -20,34 +26,30 @@ function buttonSendAction() {
 
         let timestamp = `${hour}:${minute}:${second}`;
 
-        // JSON String bilden
+        // creating the JSON String
         const chatMessageToServer = {
             username: savedNickname,
-            message: eingabeText,
+            message: inputText,
             timestamp: timestamp,
             language: chosenlanguage
         };
         let data = JSON.stringify(chatMessageToServer);
 
-        //document.getElementById("chatTextArea").innerHTML = data;
+        // sending the json String to the Server
         socket.send(data);
     }
 }
 
+/**
+ * This method is being executed when loading the page
+ * It established the connection between a client and the server
+ */
 function establishConnection() {
     socket = new WebSocket("ws://localhost:9000");
-    
-    // Connection opened
-    socket.addEventListener("open", (event) => {
-        //var data = JSON.stringify({"lang":"data"});
-        data = "test"
-        console.log(data);
-    });
-
 
     socket.addEventListener("message", (event) => {
-        //console.log("Message from server ", event.data);
-        // Parsen der empfangenen JSON-Nachricht
+
+        // parsing of the received json message
         const receivedMessage = JSON.parse(event.data);
 
         if(receivedMessage.numOfClients) {
@@ -79,7 +81,12 @@ function establishConnection() {
         }
     });
 }
-function buttonNicknameSave() {
+
+/**
+ * Declares the action that are taken after pressing the "Save" Button
+ * It includes:
+ */
+function saveUserSettings() {
 
     const nicknameEingabe = document.getElementById("nicknameArea");
 
@@ -90,93 +97,251 @@ function buttonNicknameSave() {
         nicknameEingabe.readOnly = true;
         nicknameEingabe.style.backgroundColor = "lightgrey";
 
+        //disables the dropdown menu
         let dropdowns = document.querySelectorAll('.dropdown');
         dropdowns.forEach(function (dropdown){
             dropdown.classList.add('no-hover-dropdown');
         })
-
     }
 }
 
 /**
- * Mit der Methode wird die Sprache festgelegt, auf der man die Nachrichten übersetzt haben will.
+ * This method declares which language the user selected
  */
 function selectLanguage() {
     switch (document.getElementById("dropdownValue").value){
         case "1": chosenlanguage ="de";
-                document.getElementById("chosenLanguageFlag").src = "img/Flag_of_Germany.png";
         break;
         case "2": chosenlanguage ="en";
-                document.getElementById("chosenLanguageFlag").src = "img/Flag_of_England.png";
         break;
         case "3": chosenlanguage ="fr";
-                document.getElementById("chosenLanguageFlag").src = "img/Flag_of_France.png";
         break;
         case "4": chosenlanguage = "it";
-                document.getElementById("chosenLanguageFlag").src = "img/Flag_of_Italy.png";
         break;
         case "5": chosenlanguage = "es";
-                document.getElementById("chosenLanguageFlag").src = "img/Flag_of_Spain.png";
         break;
+    }
+    changeDisplayedFlag();
+}
+
+/**
+ * This function changes the image, which is displayed in the dropdown menu of the language select button
+ */
+function changeDisplayedFlag(){
+    let element = document.getElementById("chosenLanguageFlag");
+    switch (chosenlanguage){
+        case "de": element.src = "img/Flag_of_Germany.png"; break;
+        case "en": element.src = "img/Flag_of_England.png"; break;
+        case "fr": element.src = "img/Flag_of_France.png"; break;
+        case "it": element.src = "img/Flag_of_Italy.png"; break;
+        case "es": element.src = "img/Flag_of_Spain.png"; break;
+    }
+    changeLanguageOnTheSite();
+}
+
+/**
+ * This method enables the scroll-bar on the right-side of the chat window
+ */
+function scrollToBottom() {
+    const chatTextArea = document.getElementById("chatTextArea");
+    chatTextArea.scrollTop = chatTextArea.scrollHeight;
+}
+
+/**
+ * This method changes the language of every html element to the chosen language
+ */
+function changeLanguageOnTheSite(){
+
+    let articleGuide = document.getElementById("guideArticle");
+    let chatMessageInput = document.getElementById("chatTextInput");
+    let usernameInput = document.getElementById("nicknameArea");
+    let saveButton = document.getElementById("userSettingSaveButton");
+    let sendButton = document.getElementById("sendButton");
+    let userNameLabel = document.getElementById("userNameLabel");
+    let languageLabel = document.getElementById("languageLabel");
+    let userListLabel = document.getElementById("userListLabel");
+    let chatWindow = document.getElementById("chatTextArea");
+
+    switch (chosenlanguage){
+        case "de":
+            articleGuide.innerHTML =
+                "<u>Getting Started with Virtualmeet</u><br>" +
+                "Willkommen bei Virtualmeet! Hier ist eine schnelle Anleitung, um dir den Einstieg zu erleichtern:<br>" +
+                "<br>" +
+                "<b>1. Nutzernamen und Sprache auswählen</b><br>" +
+                "Bevor du loslegst, wähle einen Nutzernamen und deine bevorzugte Sprache aus. Vergiss nicht auf „Speichern“ zu drücken<br>" +
+                "<b>2. Senden einer Nachricht</b><br>" +
+                "Um eine Nachricht zu senden, gebe einfach deinen Text in das Textfeld ein und klicke auf „Senden“. Deine Nachricht wird dann an alle anderen Chatteilnehmer gesendet.<br>" +
+                "<b>3. Beteiligung des Chatbots an der Konversation</b><br>" +
+                "Wenn du möchtest, dass der Chatbot sich an der Konversation beteiligt, erwähne einfach seinen Namen in deiner Nachricht. Unser Chatbot heißt „Alexa“. Alexa reagiert anschließen automatisch auf deine Nachricht.<br>" +
+                "Zum Beispiel: „Alexa, wie ist aktuell die Stimmung im Chat?“<br>" +
+                "<b>4. Aktive Nutzer</b><br>" +
+                "Auf der linken Seite des Bildschirms findest du eine Liste aller aktiven User. Neben dem Nutzernamen wird auch die eingestellte Sprache angezeigt.<br>" +
+                "<b>5. Anzeige der aktuellen Stimmung im Chat:</b><br>" +
+                "Hinter jeder Nachricht siehst du einen Indikator, der die aktuelle Stimmung im Chat anzeigt. Dieser Indikator wird von ganz alleine berechnet. Du musst also nichts machen!<br>" +
+                "<br>" +
+                "Das war's! Du bist nun bereit, Virtualmeet zu nutzen und mit anderen Usern zu kommunizieren. <br>" +
+                "Viel Spaß! 😊";
+            chatMessageInput.placeholder = "Nachricht eingeben...";
+            usernameInput.placeholder = "Benutzername eingeben..."
+            chatWindow.placeholder = "Bitte einen Benutzernamen angeben und eine Sprache auswählen um den Chatraum zu betreten...";
+            saveButton.textContent = "Sichern";
+            sendButton.textContent = "Senden";
+            userNameLabel.textContent = "Benutzername";
+            languageLabel.textContent = "Sprache"
+            userListLabel.textContent = "Aktive Benutzer";
+            break;
+        case "en":
+            articleGuide.innerHTML =
+                "<u>Getting Started with Virtualmeet</u><br>" +
+                "Welcome to Virtualmeet! Here's a quick guide to get you started:<br>" +
+                "<br>" +
+                "<b>1. Select username and language</b><br>" +
+                "Before you get started, choose a username and your preferred language. Don't forget to press \"Save\".<br>" +
+                "<b>2. Sending a message</b><br>" +
+                "To send a message, simply enter your text in the text field and click Send. Your message will then be sent to all other chat participants.<br>" +
+                "<b>3. Chatbot participation in the conversation</b><br>" +
+                "If you want the chatbot to join the conversation, simply mention its name in your message. Our chatbot is called “Alexa”. Alexa then automatically responds to your message.<br>" +
+                "For example: “Alexa, what is the current mood in the chat?”<br>" +
+                "<b>4. Active users</b><br>" +
+                "On the left side of the screen you will find a list of all active users. In addition to the user name, the language set is also displayed.<br>" +
+                "<b>5. Display the current mood in the chat:</b><br>" +
+                "Behind each message you will see an indicator that shows the current mood in the chat. This indicator is calculated all by itself. So you don't have to do anything!<br>" +
+                "<br>" +
+                "That's it! You are now ready to use Virtualmeet and communicate with other users.<br>" +
+                "Have fun! 😊";
+            chatMessageInput.placeholder = "Enter a message...";
+            usernameInput.placeholder = "Enter a username..."
+            chatWindow.placeholder = "Please enter a username and select a language to enter the chatroom...";
+            saveButton.textContent = "Save";
+            sendButton.textContent = "Send";
+            userNameLabel.textContent = "Username";
+            languageLabel.textContent = "Language"
+            userListLabel.textContent = "Current Users";
+            break;
+        case "fr":
+            articleGuide.innerHTML =
+                "<u>Premiers pas avec Virtual Meet</u><br>" +
+                "Bienvenue sur Rencontre Virtualmeet ! Voici un guide rapide pour vous aider à démarrer :<br>" +
+                "\n" +
+                "<b>1. Sélectionnez le nom d'utilisateur et la langue</b><br>" +
+                "Avant de commencer, choisissez un nom d'utilisateur et votre langue préférée. N'oubliez pas d'appuyer sur \"Enregistrer\".\n" +
+                "<b>2. Envoi d'un message</b><br>" +
+                "Pour envoyer un message, saisissez simplement votre texte dans le champ de texte et cliquez sur Envoyer. Votre message sera ensuite envoyé à tous les autres participants au chat.\n" +
+                "<b>3. Participation du chatbot à la conversation</b><br>" +
+                "Si vous souhaitez que le chatbot rejoigne la conversation, mentionnez simplement son nom dans votre message. Notre chatbot s'appelle « Alexa ». Alexa répond alors automatiquement à votre message.\n" +
+                "Par exemple : « Alexa, quelle est l'ambiance actuelle dans le chat ?<br>" +
+                "<b>4. Utilisateurs actifs</b><br>" +
+                "Sur le côté gauche de l'écran, vous trouverez une liste de tous les utilisateurs actifs. Outre le nom d'utilisateur, la langue définie est également affichée.<br>" +
+                "<b>5. Affichez l'ambiance actuelle dans le chat :</b><br>" +
+                "Derrière chaque message, vous verrez un indicateur qui montre l'ambiance actuelle du chat. Cet indicateur est calculé tout seul. Vous n’avez donc rien à faire !<br>" +
+                "<br>" +
+                "C'est ça! Vous êtes maintenant prêt à utiliser Virtual Meet et à communiquer avec d'autres utilisateurs.<br>" +
+                "Amusez-vous! 😊";
+            chatMessageInput.placeholder = "Entrez votre message...";
+            usernameInput.placeholder = "Saisissez votre nom d'utilisateur..."
+            chatWindow.placeholder = "Veuillez saisir un nom d'utilisateur et sélectionner une langue pour accéder au salon de discussion...";
+            saveButton.textContent = "sauver";
+            sendButton.textContent = "Envoyer";
+            userNameLabel.textContent = "Nom d'utilisateur";
+            languageLabel.textContent = "Langue"
+            userListLabel.textContent = "Utilisateurs actuels";
+            break;
+        case "it":
+            articleGuide.innerHTML =
+                "<u>Iniziare con Virtualmeet</u><br>" +
+                "Benvenuto su Virtualmeet! Ecco una guida rapida per iniziare:<br>" +
+                "<br>" +
+                "<b>1. Seleziona nome utente e lingua</b><br>" +
+                "Prima di iniziare, scegli un nome utente e la tua lingua preferita. Non dimenticare di premere \"Salva\".<br>" +
+                "<b>2. Invio di un messaggio</b><br>" +
+                "Per inviare un messaggio, inserisci semplicemente il testo nel campo di testo e fai clic su Invia. Il tuo messaggio verrà quindi inviato a tutti gli altri partecipanti alla chat.<br>" +
+                "<b>3. Partecipazione del chatbot alla conversazione</b><br>" +
+                "Se vuoi che il chatbot si unisca alla conversazione, menziona semplicemente il suo nome nel tuo messaggio. Il nostro chatbot si chiama “Alexa”. Alexa risponde quindi automaticamente al tuo messaggio.<br>" +
+                "Ad esempio: \"Alexa, qual è l'umore attuale nella chat?\"<br>" +
+                "<b>4. Utenti attivi </b><br>" +
+                "Sul lato sinistro dello schermo troverai un elenco di tutti gli utenti attivi. Oltre al nome utente viene visualizzata anche la lingua impostata.<br>" +
+                "<b>5. Visualizza lo stato d'animo attuale nella chat:</b><br>" +
+                "Dietro ogni messaggio vedrai un indicatore che mostra lo stato d'animo attuale nella chat. Questo indicatore è calcolato da solo. Quindi non devi fare nulla!<br>" +
+                "<br>" +
+                "Questo è tutto! Ora sei pronto per utilizzare Virtualmeet e comunicare con altri utenti.<br>" +
+                "Divertiti! 😊";
+            chatMessageInput.placeholder = "Inserisci il tuo messaggio...";
+            usernameInput.placeholder = "Inserire username..."
+            chatWindow.placeholder = "Inserisci un nome utente e seleziona una lingua per accedere alla chat room...";
+            saveButton.textContent = "Salva";
+            sendButton.textContent = "Inviare";
+            userNameLabel.textContent = "Nome utente";
+            languageLabel.textContent = "Lingua"
+            userListLabel.textContent = "Utenti attivi";
+            break;
+        case "es":
+            articleGuide.innerHTML =
+                "<u>Comenzando con Virtualmeet</u><br>" +
+                "¡Bienvenidos a VirtualMeet! Aquí tienes una guía rápida para empezar:<br>" +
+                "<br>" +
+                "<b>1. Seleccione nombre de usuario e idioma</b><br>" +
+                "Antes de comenzar, elija un nombre de usuario y su idioma preferido. No olvides presionar \"Guardar\".<br>" +
+                "<b>2. Enviar un mensaje</b><br>" +
+                "Para enviar un mensaje, simplemente ingrese su texto en el campo de texto y haga clic en Enviar. Luego, su mensaje se enviará a todos los demás participantes del chat.<br>" +
+                "<b>3. Participación del chatbot en la conversación. </b><br>" +
+                "Si desea que el chatbot se una a la conversación, simplemente mencione su nombre en su mensaje. Nuestro chatbot se llama \"Alexa\". Luego, Alexa responde automáticamente a su mensaje.<br>" +
+                "Por ejemplo: \"Alexa, ¿cuál es el estado de ánimo actual en el chat?\"<br>" +
+                "<b>4. Usuarios activos </b><br>" +
+                "En el lado izquierdo de la pantalla encontrarás una lista de todos los usuarios activos. Además del nombre de usuario, también se muestra el idioma configurado.<br>" +
+                "<b>5. Muestra el estado de ánimo actual en el chat:</b><br>" +
+                "Detrás de cada mensaje verás un indicador que muestra el estado de ánimo actual en el chat. Este indicador se calcula por sí solo. ¡Así que no tienes que hacer nada!<br>" +
+                "<br>" +
+                "¡Eso es todo! Ahora está listo para utilizar Virtualmeet y comunicarse con otros usuarios.<br>" +
+                "¡Divertirse! 😊";
+            chatMessageInput.placeholder = "Ingrese su mensaje...";
+            usernameInput.placeholder = "Introduzca su nombre de usuario..."
+            chatWindow.placeholder = "Por favor ingrese un nombre de usuario y seleccione un idioma para ingresar a la sala de chat...";
+            saveButton.textContent = "Ahorrar";
+            sendButton.textContent = "Enviar";
+            userNameLabel.textContent = "Usuario";
+            languageLabel.textContent = "Idioma"
+            userListLabel.textContent = "Usuarios activos";
+            break;
     }
 }
 
-function scrollToBottom() {
-    const chatTextArea = document.getElementById("chatTextArea");
-    chatTextArea.scrollTop = chatTextArea.scrollHeight; // Scrollt zum unteren Rand
-}
-
-//Nachrichten können mit "Enter"-Taste verschickt werden
+/**
+ * Adds the ability to send messages with the "Enter" button
+ */
 document.addEventListener('keydown', function(event){
-   if(event.key === "Enter"){
+    let element = document.getElementById("chatTextInput");
+    if(event.key === "Enter" && document.activeElement === element){
        document.getElementById("sendButton").click();
-   }
+    }
 });
 
-//Testbereich
+/**
+ * Adds an EventListener that is responsible for the dropdown menu of the language select button.
+ * Tt gets all HTML "a" element that are in the dropdown-content div and saves it in a NodeList.
+ * Every Element gets appointed a function that is being executed after being clicked.
+ * There the chosen value (language) gets set and will be displayed as the chosen language in the UI
+ */
 document.addEventListener('DOMContentLoaded', function() {
   var dropdownLinks = document.querySelectorAll('.dropdown-content a');
-  //var dropbtn = document.querySelector('.dropdownBtn');
   var hiddenInput = document.getElementById('dropdownValue');
 
   dropdownLinks.forEach(function(link) {
-    link.addEventListener('click', function(e) {
+      link.addEventListener('click', function(e) {
       var value = this.getAttribute('data-value');
-      //var text = this.textContent;
-     // setDropdownValue(value, text);
-      setDropdownValue(value);
+      hiddenInput.value = value;
       selectLanguage();
     });
   });
-
-  function setDropdownValue(value) {
-    hiddenInput.value = value;
-    //dropbtn.textContent = text;
-  }
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * Adds an event listener that is responsible for saving the user settings IF the user is in the input field and presses "Enter"
+ */
+document.addEventListener("keydown", function(event) {
+    let element = document.getElementById("nicknameArea");
+    if (event.key === "Enter" && document.activeElement === element) {
+        event.preventDefault();
+        saveUserSettings();
+    }
+});
