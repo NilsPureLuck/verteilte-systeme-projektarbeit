0a66cfebe2b63db51e6c3bc14aafa743aa7c0a34
iamouttacontrol <t.yilmaz0309@gmail.com>
2024-02-11 10:29:13 +0100

    added error-handling for Translator.py

--- Backend/Main/ServerFuncs/Translator.py
+++ Backend/Main/ServerFuncs/Translator.py
@@ -4,6 +4,12 @@ from google.cloud import translate_v2 as translate
 from .Message import MessageFromClient, MessageToClient
 import traceback
 
+def initialize_translate_client():
+    """
+    Initializes the Google Translate client with the necessary credentials.
+    """
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../../credentials.json"
+    return translate.Client()
 
 def translate_text(message: MessageFromClient | MessageToClient, retry=False) -> (MessageFromClient | MessageToClient, str):
     """
@@ -15,29 +21,21 @@ def translate_text(message: MessageFromClient | MessageToClient, retry=False) ->
     :return: Tuple of (translated message, detected source language or "unknown" on error)
     """
     try:
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../../credentials.json"
-        translate_client = translate.Client()
+        translate_client = initialize_translate_client()
         message_str = message.message
         target = message.language
 
-        #message.language funktioniert irgendwie nicht ganz
-
-        print("message language:  "+message.language)
         if isinstance(message_str, bytes):
             message_str = message_str.decode("utf-8")
 
-        print("message: -> "+ message_str)
-        print("target language: -> "+ target) # target bekommt er nciht?
-        result = translate_client.translate(message_str, target_language='en')
+        result = translate_client.translate(message_str, target_language=target)
         result["translatedText"] = html.unescape(result["translatedText"])
         message.message = result["translatedText"]
-        return message, result["detectedSourceLanguage"]
+        return message, result.get("detectedSourceLanguage", "unknown")
     except Exception as e:
         print("An error occurred while translating the text:", e)
-        print(traceback.format_exc())
         if not retry:
             print("Retrying...")
             return translate_text(message, retry=True)
         else:
-            # Return the original message and "unknown" if the retry also fails
             return message, "unknown"
